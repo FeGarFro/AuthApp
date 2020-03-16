54bc6ce6ac9ad79bfeb85f2fd51f743a33264088
Felipe <felipe.garcia@hotmail.com.br>
2020-03-16 00:36:19 -0300

    some changes in DB models

--- models/user.js
+++ models/user.js
@@ -3,11 +3,30 @@ const mongoose = require("mongoose");
 
 //Collection Init
 const userSchema = mongoose.Schema({
+    profilePic: {type: Image},
     name:{type: String},
-    email:{type: String, required: true},
-    username:{type: String, required: true},
-    password:{type: String, required: true},
-    
+    email:{type: String},
+    username:{type: String},
+    password:{type: String},
+    doc: {type: String},
+    phone:{type: String},
+    tags: {type: Object},
+    bookshelf: {type: Book}
 });
+
+const Book = {
+    local_id: {type: Number},
+    api_id: {type: String},
+    title: {type: String},
+    author: {type: String},
+    thumbnail: {type: String},
+    status: {type: String},
+    history: {
+        checkpoint:{
+            date: {type: Date},
+            page: {type: String}
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
